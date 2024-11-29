dcd5082fabe66baa0745a1e1cd5c17c9f14741dd
Aniket <132605075+anni-x1@users.noreply.github.com>
2024-11-29 11:13:05 +0530

    added remote server url

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:56000/api/auth/login', {
+      const response = await fetch('https://bluechat-backend.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
